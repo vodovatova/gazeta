d5d47a57ada914784db915f8b348c691bd0489bf
vodovatova <pvodovatova@bk.ru>
2016-11-21 20:55:07 +0300

    Delete 3.py

--- 3.py
+++
@@ -1,9 +0,0 @@
-import crawler
-
-c = crawler.Crawler(metadata=False)
-data = c.get_data("http://www.vestnik.udm.net/?p=2151")
-
-print(
-    "Название: %s\nАвтор: %s\nДата публикации: %s\nКатегории: %s\n"
-    % (data['header'], data['author'], data['date'], data['topic'])
-)
